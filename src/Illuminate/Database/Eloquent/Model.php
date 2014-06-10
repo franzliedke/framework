9fb6128e0fcd64bef851fc01c49cb522ede02e3f
Franz Liedke <franz@develophp.org>
2014-06-10 19:07:40 +0200

    Move setting of exists variable.

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -1105,8 +1105,6 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 
 			$this->performDeleteOnModel();
 
-			$this->exists = false;
-
 			// Once the model has been deleted, we will fire off the deleted event so that
 			// the developers may hook into post-delete operations. We will then return
 			// a boolean true as the delete is presumably successful on the database.
@@ -1124,6 +1122,8 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	protected function performDeleteOnModel()
 	{
 		$this->newQuery()->where($this->getKeyName(), $this->getKey())->delete();
+
+		$this->exists = false;
 	}
 
 	/**
